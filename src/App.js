ca0a4a1641f8b4a781b5c713f812fd7946bce48f
Phuoc205 <tanphuoc120605@gmail.com>
2025-11-20 15:52:33 +0700

    Initial code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react' // nạp thư viện react
+import { HashRouter as BrowserRouter, Routes, Route } from 'react-router-dom'
+
+
+function App () {
+    return (
+        <BrowserRouter>
+
+        </BrowserRouter>
+    )
+}
+
+export default App
